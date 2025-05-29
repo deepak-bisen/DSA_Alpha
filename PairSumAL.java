34e7d5403dbb3c44954a3379c43a8baa9e5586ff
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-30 02:00:51 +0530

    Update PairSumAL.java

--- PairSumAL.java
+++ PairSumAL.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 //find if any pair in a sorted ArrayList has a target sum. return true if any.
 public class PairSumAL {
 	//Brute Force
-	public static boolean pairSum(ArrayList<Integer> list, int target) {
+	public static boolean pairSumBF(ArrayList<Integer> list, int target) {
 		for(int i = 0; i < list.size()-1; i++) {
 			for(int j = i+1; j < list.size()-1; j++) {
 				if(target == (list.get(i)+list.get(j)))
@@ -12,8 +12,27 @@ public class PairSumAL {
 		}
 		return false;
 	}
+	
+	// 2-Pointer
+	public static boolean pairSum2P(ArrayList<Integer> list, int target) {
+		int leftPointer = 0;
+		int rightPointer = list.size() - 1;
+		while (leftPointer < rightPointer) {
+		int currentSum =list.get(leftPointer) + list.get(rightPointer);
+			// case 1:
+			if ( currentSum == target )
+				return true;   //pair found
+			// case 2:
+			else if (currentSum < target)
+				leftPointer++;  //increase sum
+			// case 3:
+			else
+				rightPointer--;  //decrease sum
+		}
+		return false;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(2);
@@ -21,8 +40,9 @@ public class PairSumAL {
 		list.add(4);
 		list.add(5);
 		list.add(6);
-		 
-		System.out.println(pairSum(list,5));
+		 int target = 5;
+//		System.out.println(pairSumBF(list,target));
+		System.out.println(pairSum2P(list,target));
 	}
 
 }
