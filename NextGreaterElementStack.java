777cf31babeb66bdf1db3123bca02dbc848d8c39
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-14 11:58:31 +0530

    Update NextGreaterElementStack.java

--- NextGreaterElementStack.java
+++ NextGreaterElementStack.java
@@ -1,7 +1,9 @@
 package alpha;
 
+import java.util.Stack;
+
 public class NextGreaterElementStack {
-	//brute force  
+	//brute force  0(n^2)
 	public static void nextGE(int arr[]) {
 	int 	arr1[]  = new int[arr.length];
 		for(int i =0; i < arr.length ;i++) {
@@ -24,8 +26,31 @@ public class NextGreaterElementStack {
 			System.out.println(a);
 		}
 	}
+	//optimised using stack O(n)
+	public static void nextGreaterElementStack(int arr[]) {
+		Stack<Integer> s = new Stack<>();
+		int nge[] = new int[arr.length];
+	for(int i = arr.length-1; i>= 0; i--) {
+		//1 while
+		while(!s.isEmpty() && arr[s.peek()]<=arr[i]) {
+			s.pop();
+		}
+		//2 if-else
+		if(s.isEmpty())
+			nge[i] = -1;
+		else
+			nge[i] = arr[s.peek()];
+		//3 push
+		s.push(i);
+			
+	}
+	for (int a  : nge) {
+		System.out.println(a);
+	}
+	}
 public static void main(String[] args) {
 	int arr[] = {6,8,0,1,3};
-	nextGE(arr);
+	nextGreaterElementStack(arr); 
+	//nextGE(arr);
 }
 }
