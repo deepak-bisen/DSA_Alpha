ae706f929c838aa5c7de64123d68466c5d444da3
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-27 23:39:04 +0530

    Create MinAbsoluteDifferencePairsGreedy.java

---
+++ MinAbsoluteDifferencePairsGreedy.java
@@ -0,0 +1,18 @@
+package alpha;
+import java.util.Arrays;
+public class MinAbsoluteDifferencePairsGreedy {
+public static void main(String[] args) {
+	int A[] = {4,1,8,7};
+	int B[] = {2,3,6,5};
+	
+	//sort them first for greedy approach
+	Arrays.sort(A);
+	Arrays.sort(B);
+	
+	int minAbsDiff = 0;
+	for(int i = 0; i<A.length; i++) {
+		minAbsDiff += Math.abs(A[i]-B[i]);
+	}
+	System.out.println("minimum absolute difference of pairs = "+minAbsDiff);
+}
+}
