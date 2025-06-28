d898fd4b47b1f063ac112a6c2cb64c12c4e78256
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-29 01:20:33 +0530

    Update IndianCoinsGreedy.java

--- IndianCoinsGreedy.java
+++ IndianCoinsGreedy.java
@@ -1,5 +1,6 @@
 package alpha;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -8,22 +9,29 @@ public class IndianCoinsGreedy {
 
 		// coins refers to indian currency notes/coins
 		Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 };
-		int amount = 590;
 
 		// sort the currency in descending order
 		Arrays.sort(coins, Comparator.reverseOrder());
 
 		int count = 0;
+		int amount = 590;
+		ArrayList<Integer> ans = new ArrayList<Integer>();
 
 		for (int i = 0; i < coins.length; i++) {
 			if (coins[i] < amount) {
 				while (coins[i] <= amount) {
 					count++;
+					ans.add(coins[i]);
 					amount -= coins[i];
 				}
 			}
 		}
 
-		System.out.println("Total coins/notes used: " + count);
+		System.out.println("Total (min) coins/notes used: " + count);
+	
+		System.out.println("Coins/Notes Used: ");
+		for(Integer a : ans) {
+			System.out.print(a+" ");
+		}
 	}
 }
