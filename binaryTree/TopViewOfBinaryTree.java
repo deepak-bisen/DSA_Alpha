7a298173f4a65ba6de4efeb21e10fb4be3088c58
Deepak14-12 <bisen1412@gmail.com>
2025-07-09 19:16:26 +0530

    created TopViewOfBinaryTree.java

---
+++ binaryTree/TopViewOfBinaryTree.java
@@ -0,0 +1,25 @@
+package DSA_Alpha.binaryTree;
+
+import DSA_Alpha.binaryTree.SubtreeOfAnotherTree.Node;
+
+public class TopViewOfBinaryTree {
+
+	public static void main(String[] args) {
+		/**
+		 *     1
+		 *    / \
+		 *   2   3
+		 *  / \ / \
+		 * 4  5 6  7
+		 */
+
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+	}
+
+}
