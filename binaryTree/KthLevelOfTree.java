c7e0a26bb98550fea8783a560f3532847a389e58
Deepak14-12 <bisen1412@gmail.com>
2025-07-11 11:08:26 +0530

    created KthLevelOfBinaryTree.java

---
+++ binaryTree/KthLevelOfTree.java
@@ -0,0 +1,18 @@
+package DSA_Alpha.binaryTree;
+
+import DSA_Alpha.binaryTree.TraversalBinaryTree.Node;
+
+public class KthLevelOfTree {
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		public Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+}
