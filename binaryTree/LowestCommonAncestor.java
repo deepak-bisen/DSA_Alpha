9f029004f2a7fe92949740ce943803a1562da1f2
Deepak14-12 <bisen1412@gmail.com>
2025-07-12 09:48:04 +0530

    created LowestCommonAncestor.java

---
+++ binaryTree/LowestCommonAncestor.java
@@ -0,0 +1,56 @@
+package DSA_Alpha.binaryTree;
+
+import java.util.ArrayList;
+
+import DSA_Alpha.binaryTree.KthLevelOfBinaryTree.Node;
+
+public class LowestCommonAncestor {
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		public Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
+	public static Node lca(Node root, int n1, int n2) {
+		ArrayList<Node> path1 = new ArrayList<>();
+		ArrayList<Node> path2 = new ArrayList<>();
+		
+		getPath(root,n1,path1);
+		getPath(root,n2,path2);
+		
+		//last common ancestor
+		int i =0;
+		for(;i<path1.size() && path2.size();i++) {
+			if(path1.get(i) != path2.get(i))
+				break;
+		}
+		
+		//last equal node --> i-1 th
+		Node lca = path1.get(i-1);
+		return lca;
+	}
+	public static void main(String[] args) {
+		/**
+		 *     1
+		 *    / \
+		 *   2   3
+		 *  / \ / \
+		 * 4  5 6  7
+		 */
+
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+}}
+
