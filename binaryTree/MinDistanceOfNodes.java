aea99819754320a694dcc4927761e15b08dc5540
Deepak14-12 <bisen1412@gmail.com>
2025-07-13 12:34:38 +0530

    completed MinDistanceOfNodes.java

--- binaryTree/MinDistanceOfNodes.java
+++ binaryTree/MinDistanceOfNodes.java
@@ -43,13 +43,21 @@ public class MinDistanceOfNodes {
 		if(root.data == n)
 			return 0;
 		
-		if()
+		int leftDist = lcaDist(root.left, n);
+		int rightDist = lcaDist(root.right,n);
+		
+		if(leftDist == -1 && rightDist == -1)
+			return -1;
+		else if(leftDist == -1)
+			return rightDist+1; //at this time we will be on upper level of node that's why +1 in distance value  
+		else
+			return leftDist+1;  //at this time we will be on upper level of node that's why +1 in distance value
 	}
 	
 	public static int minDist(Node root, int n1, int n2) {
 		Node lca = lca2(root,n1,n2);
 		int dist1 = lcaDist(lca,n1);
-		int dist2 = lcaDist2(lca,n2);
+		int dist2 = lcaDist(lca,n2);
 		
 		return dist1 + dist2; 
 	}
@@ -72,5 +80,9 @@ public class MinDistanceOfNodes {
 		
 		int n1  = 4, n2 = 6;
 		
+		System.out.println("Mininum of Distance Between "+n1+" And "+n2+" : "+
+				minDist(root, n1, n2));
+		
+		System.out.println("Min distance between two node = number of edges between them.");
     }
 }
