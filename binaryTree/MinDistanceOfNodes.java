03d04db7da98eb65594411f26da98d4aef2c32e6
Deepak14-12 <bisen1412@gmail.com>
2025-07-13 12:21:34 +0530

    created MinDistanceOfNodes.java

--- binaryTree/MinDistanceOfNodes.java
+++ binaryTree/MinDistanceOfNodes.java
@@ -1,5 +1,7 @@
 package DSA_Alpha.binaryTree;
 
+import DSA_Alpha.binaryTree.LowestCommonAncestor.Node;
+
 public class MinDistanceOfNodes {
 	static class Node {
 		int data;
@@ -13,4 +15,43 @@ public class MinDistanceOfNodes {
 		}
 	}
 	
+//	Approach 2
+	public static Node lca2(Node root, int n1, int n2) {
+		if(root == null || root.data == n1 || root.data == n2)
+			return root;
+		
+		Node leftLca = lca2(root.left, n1, n2);
+		Node rightLca = lca2(root.right,n1,n2);
+		
+		//leftLCA = value  rightLCA = null
+		if(rightLca == null)
+			return leftLca;
+		
+		//rightLCA = value leftLCA = null
+		if(leftLca == null)
+			return rightLca;
+		
+		return root;
+				
+	}
+	public static void main(String[] args) {
+		/**
+		 *     1
+		 *    / \
+		 *   2   3
+		 *  / \ / \
+		 * 4  5 6  7
+		 */
+
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		
+		int n1  = 4, n2 = 6;
+		
+    }
 }
