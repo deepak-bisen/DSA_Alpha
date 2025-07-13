5eb778b5248fed802e59bef2982d2ac4547faae2
Deepak14-12 <bisen1412@gmail.com>
2025-07-13 12:20:14 +0530

    created MinDistanceOfNodes.java

---
+++ binaryTree/MinDistanceOfNodes.java
@@ -0,0 +1,16 @@
+package DSA_Alpha.binaryTree;
+
+public class MinDistanceOfNodes {
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		public Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
+}
