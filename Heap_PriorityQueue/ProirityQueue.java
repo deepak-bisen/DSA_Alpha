36be81dfa3456e60571b1423d41b966ddeda685b
Deepak14-12 <bisen1412@gmail.com>
2025-09-17 01:06:43 +0530

    updaated

--- Heap_PriorityQueue/ProirityQueue.java
+++ Heap_PriorityQueue/ProirityQueue.java
@@ -1,19 +1,36 @@
 package DSA_Alpha.Heap_PriorityQueue;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class ProirityQueue {
-	public static void main(String[] args) {
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+	static class Student implements Comparable<Student> {
+		String name;
+		int rank;
 
-		pq.add(2);
-		pq.add(5);
-		pq.add(1);
-		pq.add(3);
-		pq.add(4);
+		public Student(String name, int rank) {
+			this.name = name;
+			this.rank = rank;
+		}
+
+		@Override
+		public int compareTo(Student s) {
+			return this.rank - s.rank;
+		}
+	}
+
+	public static void main(String[] args) {
+		PriorityQueue<Student> pq = new PriorityQueue<Student>();
+//		PriorityQueue<Student> pq = new PriorityQueue<Student>(Comparator.reverseOrder());
+
+		pq.add(new Student("A", 1));
+		pq.add(new Student("B", 8));
+		pq.add(new Student("C", 3));
+		pq.add(new Student("D", 5));
+		pq.add(new Student("E", 2));
 
 		while (!pq.isEmpty()) {
-			System.out.println(pq.peek());
+			System.out.println(pq.peek().name+" --> "+pq.peek().rank);
 			pq.remove();
 
 		}
